39f5ecaebd8fc63d1ee312d4bc6a1901f530cb66
Mark Vujevits <mark.vujevits@prezi.com>
2018-03-17 13:43:54 +0100

    use futures for async calls

--- src/main/kotlin/co/helmethair/CurrencyLayerApi.kt
+++ src/main/kotlin/co/helmethair/CurrencyLayerApi.kt
@@ -1,9 +1,14 @@
 package co.helmethair
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import java.math.BigDecimal
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
+
 
 object ParamFields {
     const val ACCESS_KEY = "access_key"
@@ -24,7 +29,31 @@ data class ListResponse(
     val currencies: Map<String, String>
 )
 
+data class ConvertResponse(
+    val success: Boolean,
+    val terms: String,
+    val privacy: String,
+    val query: ConvertQuery,
+    val info: ConvertInfo,
+    val result: Double
+)
+
+data class ConvertQuery(
+    val from: String,
+    val to: String,
+    val amount: Double
+)
+
+
+data class ConvertInfo(
+    val timestamp: Long,
+    val quote: Double
+)
+
 class CurrencyLayerApi(private val accessKey: String) {
+    private val jsonMapper: ObjectMapper = ObjectMapper().registerKotlinModule()
+    private val cachedThreadPool = Executors.newCachedThreadPool()
+
     companion object {
         private const val BASE_URL = "http://apilayer.net/api/"
     }
@@ -33,7 +62,7 @@ class CurrencyLayerApi(private val accessKey: String) {
         FuelManager.instance.basePath = BASE_URL
     }
 
-    fun list(): String {
+    fun list(): ListResponse {
         val (request, response, result) = Endpoints.LIST.httpGet(
             parameters = listOf(
                 ParamFields.ACCESS_KEY to accessKey
@@ -42,7 +71,7 @@ class CurrencyLayerApi(private val accessKey: String) {
 
         when (result) {
             is Result.Success -> {
-                return result.value
+                return jsonMapper.readValue(result.value, jacksonTypeRef<ListResponse>())
             }
             is Result.Failure -> {
                 throw result.error
@@ -50,30 +79,19 @@ class CurrencyLayerApi(private val accessKey: String) {
         }
     }
 
-    fun listAsync(onSuccess: (result: String) -> Unit, onError: (e: Exception) -> Unit) {
-        Endpoints.LIST.httpGet(
-            parameters = listOf(
-                    ParamFields.ACCESS_KEY to accessKey
-            ))
-            .responseString { request, response, result ->
-                when (result) {
-                    is Result.Success -> {
-                        onSuccess(result.get())
-                    }
-                    is Result.Failure -> {
-                        onError(result.getException())
-                    }
-                }
-            }
+    fun listAsync(): CompletableFuture<ListResponse> {
+        return calculateAsync(CompletableFuture<ListResponse>()) { future ->
+            listAsyncBlock(future)
+        }
     }
 
-    fun convert(amount: BigDecimal, fromCurrency: String, toCurrency: String): String {
+    fun convert(query: ConvertQuery): String {
         val (request, response, result) = Endpoints.CONVERT.httpGet(
             parameters = listOf(
                 ParamFields.ACCESS_KEY to accessKey,
-                ParamFields.FROM to fromCurrency,
-                ParamFields.TO to toCurrency,
-                ParamFields.AMOUNT to amount
+                ParamFields.FROM to query.from,
+                ParamFields.TO to query.to,
+                ParamFields.AMOUNT to query.amount
             ))
             .responseString()
 
@@ -87,23 +105,55 @@ class CurrencyLayerApi(private val accessKey: String) {
         }
     }
 
-    fun convertAsync(amount: BigDecimal, fromCurrency: String, toCurrency: String, onSuccess: (result: String) -> Unit, onError: (e: Exception) -> Unit) {
+    fun convertAsync(query: ConvertQuery): CompletableFuture<ConvertResponse> {
+        return calculateAsync(CompletableFuture<ConvertResponse>()) { future ->
+            convertAsyncBlock(future, query)
+        }
+    }
+
+    private fun <T>calculateAsync(future: CompletableFuture<T>, block: (future: CompletableFuture<T>) -> CompletableFuture<T>): CompletableFuture<T> {
+        cachedThreadPool.submit { block(future) }
+        return future
+    }
+
+    private fun listAsyncBlock(future: CompletableFuture<ListResponse>): CompletableFuture<ListResponse> {
+        Endpoints.LIST.httpGet(
+                parameters = listOf(
+                        ParamFields.ACCESS_KEY to accessKey
+                ))
+                .responseString { request, response, result ->
+                    when (result) {
+                        is Result.Success -> {
+                            val listObject = jsonMapper.readValue<ListResponse>(result.get(), jacksonTypeRef<ListResponse>())
+                            future.complete(listObject)
+                        }
+                        is Result.Failure -> {
+                            throw result.error
+                        }
+                    }
+                }
+        return future
+    }
+
+    private fun convertAsyncBlock(future: CompletableFuture<ConvertResponse>, query: ConvertQuery): CompletableFuture<ConvertResponse> {
         Endpoints.CONVERT.httpGet(
             parameters = listOf(
                 ParamFields.ACCESS_KEY to accessKey,
-                ParamFields.FROM to fromCurrency,
-                ParamFields.TO to toCurrency,
-                ParamFields.AMOUNT to amount
+                ParamFields.FROM to query.from,
+                ParamFields.TO to query.to,
+                ParamFields.AMOUNT to query.amount
             ))
             .responseString { request, response, result ->
                 when (result) {
                     is Result.Success -> {
-                        onSuccess(result.get())
+                        val convertObject = jsonMapper.readValue<ConvertResponse>(result.get(), jacksonTypeRef<ConvertResponse>())
+                        future.complete(convertObject)
                     }
                     is Result.Failure -> {
-                        onError(result.getException())
+                        throw result.error
                     }
                 }
             }
+        return future
     }
 }
